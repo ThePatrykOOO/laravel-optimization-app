a8911b1d59b9a3fa63cb6b17f5b92cf5e5011632
pfilipiak <patrykfilipiak@outlook.com>
2022-04-23 15:51:07 +0200

    dump autoload

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
     public function categoryListWithProduct(): AnonymousResourceCollection
     {
-        $categoryListWithProduct = Category::query()
-            ->with('products')
-            ->get();
-        return CategoryResource::collection($categoryListWithProduct);
+        return Cache::remember('CategoryController.categoryListWithProduct', now()->addHour(), function () {
+            $categoryListWithProduct = Category::query()
+                ->with('products')
+                ->get();
+
+            return CategoryResource::collection($categoryListWithProduct);
+        });
     }
 }
