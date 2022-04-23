82eab24486ecde5c3557cbf88b30d07e9fb5d62b
pfilipiak <patrykfilipiak@outlook.com>
2022-04-23 15:14:14 +0200

    n+1

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
-use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class CategoryController extends Controller
 {
-    public function categoryListWithProduct()
+    public function categoryListWithProduct(): AnonymousResourceCollection
     {
         $categoryListWithProduct = Category::query()
-            ->with('products')
+//            ->with('products')
             ->get();
 
         return CategoryResource::collection($categoryListWithProduct);
