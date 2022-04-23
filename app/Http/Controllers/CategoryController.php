2b5378702c6d4ef629d3eb9a087a4b4b8774fab7
pfilipiak <patrykfilipiak@outlook.com>
2022-04-23 15:27:34 +0200

    cache::remember

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -5,15 +5,18 @@ namespace App\Http\Controllers;
 use App\Http\Resources\CategoryResource;
 use App\Models\Category;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Cache;
 
 class CategoryController extends Controller
 {
     public function categoryListWithProduct(): AnonymousResourceCollection
     {
-        $categoryListWithProduct = Category::query()
-            ->with('products')
-            ->get();
+        return Cache::remember('CategoryController.categoryListWithProduct', now()->addHour(), function () {
+            $categoryListWithProduct = Category::query()
+                ->with('products')
+                ->get();
 
-        return CategoryResource::collection($categoryListWithProduct);
+            return CategoryResource::collection($categoryListWithProduct);
+        });
     }
 }
